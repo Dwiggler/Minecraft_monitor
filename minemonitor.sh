e336d97f3171340c8d06aed66be0b55a444f01e8
Dwight A <tanacioustwinkie@live.com>
2022-09-07 19:36:32 -0500

    Rough draft of script with no testing of code yet.

--- minemonitor.sh
+++ minemonitor.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
 #start of file
 #script will check to see if serivice and process is running. If not, attempt to restart and email admin
-line of code
+
+service_check () {
+        ps -ef | grep mine | grep -v grep | grep -v SCREEN | wc -l
+}
+
+process=$(ps -ef | grep mine | grep -v grep | grep -v SCREEN | wc -l)
+screen=$(ps -ef | grep mine | grep -v grep | grep -v .jar | wc -l)
+
+if [$screen -eq 0 && $process -eq 0]{
+  service minecraft start
+  #servrestart=$(true)
+
+else
+  if [$screen -eq 1 && $process -eq 0]{
+    service minecraft stop
+    x=0
+    while [ $x -gt 0 ]
+    do
+            x=$(service_check)
+    done
+    service minecraft start
+    #servrestart=$(true)
+  }
+}
+
+#if [ $servrestart -eq "true"]{
+#  echo "The service was restarted on " date %F-%H >> /tmp/minemonitor.out
+#}
